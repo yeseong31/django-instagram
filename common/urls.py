29b0fa3cc6a80627ab50436976f231eb0fd3b893
yeseong31 <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>
2022-07-14 18:55:05 +0900

    0714 signin & signup layout

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from common.views import SignupView
+from common.views import SignupView, SigninView
 
 app_name = 'common'
 
 urlpatterns = [
-    path('signup/', SignupView.as_view()),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('signin/', SigninView.as_view(), name='signin'),
 ]
--- common/views.py
+++ common/views.py
@@ -6,5 +6,13 @@ from common.models import CustomUser
 
 class SignupView(APIView):
     """회원가입"""
+
     def get(self, request):
         return render(request, 'common/signup.html')
+
+
+class SigninView(APIView):
+    """로그인"""
+
+    def get(self, request):
+        return render(request, 'common/signin.html')
