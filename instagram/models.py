0a4955ac4e92d7663ba296e0586e9ca69e32200e
예성 <yeseong31@naver.com>
2022-07-18 22:12:54 +0900

    0718 add a 'like' Feature

--- instagram/models.py
+++ instagram/models.py
@@ -10,7 +10,7 @@ class Feed(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='feeds', verbose_name='게시글 작성자')
     content = models.TextField(verbose_name='게시글 내용')
     image = models.ImageField(verbose_name='게시글 이미지', upload_to='image/%Y/%m/%d/', default='default.jpg')
-    voter = models.ManyToManyField(CustomUser, verbose_name='추천인', related_name='voter_f')
+    voter = models.ManyToManyField(CustomUser, verbose_name='추천인', related_name='voter_feed')
     create_date = models.DateTimeField(verbose_name='생성일', default=timezone.now)
     modify_date = models.DateTimeField(verbose_name='수정일', null=True, blank=True)
 
@@ -26,7 +26,7 @@ class Comment(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='comments', verbose_name='답글 작성자')
     feed = models.ForeignKey(Feed, on_delete=models.CASCADE, verbose_name='답글을 단 게시글')
     content = models.TextField(verbose_name='답글 내용')
-    voter = models.ManyToManyField(CustomUser, verbose_name='추천인', related_name='voter_c')
+    voter = models.ManyToManyField(CustomUser, verbose_name='추천인', related_name='voter_comment')
     create_date = models.DateTimeField(verbose_name='생성일')
     modify_date = models.DateTimeField(null=True, blank=True, verbose_name='수정일')
 
--- instagram/views/comment_views.py
+++ instagram/views/comment_views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404, resolve_url, redirect, render
 from django.utils import timezone
+from rest_framework.status import HTTP_200_OK
 
 from instagram.forms import CommentForm
-from instagram.models import Feed
+from instagram.models import Feed, Comment
 
 
 @login_required(login_url='common:signin')
@@ -20,7 +21,23 @@ def comment_create(request, feed_pk):
             comment.feed = feed
             comment.create_date = timezone.now()
             comment.save()
-            return redirect(f'{resolve_url("instagram:home")}#comment_{comment.pk}')
+            return redirect(f'{resolve_url("instagram:home")}#feed_{feed_pk}')
         return render(request, 'instagram/feed.html')
     else:
         return HttpResponseNotAllowed('Allow POST requests only.')
+
+
+@login_required(login_url='common:signin')
+def comment_vote(request, comment_pk):
+    """답글 추천"""
+
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    if request.user == comment.author:
+        context = {'message': '본인의 답글은 추천할 수 없습니다.',
+                   'url': f'{resolve_url("instagram:home")}#comment_{comment_pk}'}
+        return render(request, 'message.html', context, status=HTTP_200_OK)
+    if request.user in comment.voter.all():
+        comment.voter.remove(request.user)
+    else:
+        comment.voter.add(request.user)
+    return redirect(f'{resolve_url("instagram:home")}#comment_{comment_pk}')
